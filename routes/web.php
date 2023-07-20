516d6da438c0f94b8487c6aedd361bfc53b50897
Fatlinda <fatlindabrahaj@gmail.com>
2023-07-20 18:02:32 +0200

    new project

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\CarsController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductsController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::resource('/index',CarsController::class);
